d3f6d17baa122fab76edc509eb6d6f4d043a8810
FFeldmannR <fabio.feldmann97@gmail.com>
2023-05-29 09:52:02 -0300

    teste de vibração manual com botão

--- app/src/main/java/br/feldmann/controlebateria/MainActivity.java
+++ app/src/main/java/br/feldmann/controlebateria/MainActivity.java
@@ -7,7 +7,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 //
 public class MainActivity extends AppCompatActivity {
@@ -27,10 +30,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("Construtor", "onCreate");
         //
-
+        Button btnVibrar = (Button) findViewById(R.id.btnVibrar);
+        btnVibrar.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                vibrar(); //chamada do metodo vibrar
+            }
+        });
         // EXIBE NIVEL DE BATERIA
         batteryText = (TextView) findViewById(R.id.bateria);
         this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         //
     }//fim onCreate
+    public void vibrar(){
+        // Obtenha uma referência ao serviço de vibração do sistema
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        // Verifique se o dispositivo suporta vibração
+        if (vibrator.hasVibrator()) {
+            // Defina a duração da vibração em milissegundos
+            long duration = 500; // 0,5 segundo
+
+            // Vibra o dispositivo pelo tempo especificado
+            vibrator.vibrate(duration);
+        }
+    }
 }//fim class
