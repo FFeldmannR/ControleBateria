2c028b9d7d2a1ce5ed28c6c2f3c18c93ac2a7ee3
FFeldmannR <fabio.feldmann97@gmail.com>
2023-06-01 11:52:30 -0300

    separado battery receiver em outra classe

---
+++ app/src/main/java/br/feldmann/controlebateria/BatteryReceiver.java
@@ -0,0 +1,56 @@
+package br.feldmann.controlebateria;
+//
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.BatteryManager;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+//
+public class BatteryReceiver extends BroadcastReceiver {
+    private int bateria;
+    private TextView tvBateria;
+    //
+    public int getBateria() { return bateria; }
+    public void setTextView(TextView tvBateria){
+        this.tvBateria = tvBateria;
+    }
+    //
+    @Override public void onReceive(Context context, Intent intent) {
+        //
+        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+        int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        float batteryPct = level * 100 / (float)scale;
+        this.bateria = (int) batteryPct;
+        Log.d("LogTag", "bateria em receiver: "+batteryPct);
+
+        // EXIBE NIVEL DE BATERIA
+        Log.d("LogTag", "bateria no textview: "+ batteryPct );
+        tvBateria.setText( batteryPct + "%" );
+        //
+        //
+        if (isCharging && batteryPct == 100){
+            // O dispositivo está carregando e a bateria atingiu 100%
+
+            // Exemplo de alerta via Toast
+            Toast.makeText(context, "Bateria em 100%!", Toast.LENGTH_LONG).show();
+
+            // Exemplo de vibração incessante
+            Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            if (vibrator != null && vibrator.hasVibrator()) {
+                vibrator.vibrate(new long[]{0, 1000}, 0);
+            }
+        }else {
+            Toast.makeText(context, "Bateria Carregando", Toast.LENGTH_LONG).show();
+        }
+        //
+    }//fim metodo onReceive
+}//fim class
--- app/src/main/java/br/feldmann/controlebateria/MainActivity.java
+++ app/src/main/java/br/feldmann/controlebateria/MainActivity.java
@@ -1,90 +1,26 @@
 package br.feldmann.controlebateria;
 //
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.BatteryManager;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.util.Log;
 import android.widget.TextView;
 //
 public class MainActivity extends AppCompatActivity {
-    private TextView batteryText;
-    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
-        @Override public void onReceive(Context context, Intent intent) {
-            //
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-            float batteryPct = level * 100 / (float)scale;
-            verificaSeCarregando( batteryPct );
-            batteryText.setText( batteryPct + "%" );
-        }
-    };
+    private BatteryReceiver batteryReceiver;
     //
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Construtor", "onCreate");
-        // EXIBE NIVEL DE BATERIA
-        batteryText = (TextView) findViewById(R.id.bateria);
-        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         //
-        //
-        /* teste de vibração com botão
-        Button btnVibrar = (Button) findViewById(R.id.btnVibrar);
-        btnVibrar.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View view) {
-                vibrar(); //chamada do metodo vibrar
-            }
-        }); */
-    }//fim onCreate
-    //
-    public void verificaSeCarregando( float nivelBateria ){
-        // Obtenha o contexto do aplicativo
-        Context context = getApplicationContext();
+        batteryReceiver = new BatteryReceiver();
 
-        // Crie um IntentFilter para capturar a ação de alteração do estado da bateria
         IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
 
-        // Registre o receptor de broadcast para receber informações sobre a bateria
-        Intent batteryStatus = context.registerReceiver(null, intentFilter);
+        batteryReceiver.setTextView( findViewById(R.id.bateria) );
 
-        // Verifique se o dispositivo está conectado à fonte de energia
-        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                status == BatteryManager.BATTERY_STATUS_FULL;
-
-        if (isCharging) {
-            // O dispositivo está carregando
-            Log.d("LogTag", "esta carregando");
-            Log.d("LogTag", "bateria: " + nivelBateria );
-            if ( nivelBateria >= 80){
-                //chama vibrar
-                Log.d("LogTag", "bateria carregada");
-                do {
-                    vibrar();
-                }while (isCharging);
-            }
-        } else {
-            // O dispositivo não está carregando
-            Log.d("LogTag", "NÃO esta carregando");
-        }
-    }
-    //
-    public void vibrar(){
-        // Obtenha uma referência ao serviço de vibração do sistema
-        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
-        // Verifique se o dispositivo suporta vibração
-        if (vibrator.hasVibrator()) {
-            // Defina a duração da vibração em milissegundos
-            long duration = 1000; // 0,5 segundo
-
-            // Vibra o dispositivo pelo tempo especificado
-            vibrator.vibrate(duration);
-        }
-    }
+        registerReceiver(batteryReceiver, intentFilter);
+    }//fim onCreate
 }//fim class
